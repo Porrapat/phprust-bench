e5d41c8c17b899666dd20378533cf7d8899c9581
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2025-11-12 12:13:40 +0700

    Add code

--- client/main.rs
+++ client/main.rs
@@ -39,7 +39,7 @@ async fn main() {
 
     let endpoint = Endpoint::from_shared(server_addr.to_string())
         .unwrap()
-        .timeout(Duration::from_millis(300))
+        .timeout(Duration::from_secs(10))
         .connect_timeout(Duration::from_millis(100));
 
     let client_result = endpoint.connect().await;
